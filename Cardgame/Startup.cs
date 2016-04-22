9de1d4f23eb9b541e447e5ecabf17ad72074040c
Nortshango <nortshango@gmx.de>
2016-04-23 00:51:39 +0200

    Cardgame V1

---
+++ Cardgame/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cardgame.Startup))]
+namespace Cardgame
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
